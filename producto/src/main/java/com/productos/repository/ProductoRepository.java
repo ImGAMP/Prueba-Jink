b525c266b724f02d534f4acbfaf43df004262e33
Gustavo Mojica <55957159+ImGAMP@users.noreply.github.com>
2025-04-25 16:20:04 -0500

    Update ProductoRepository.java

--- producto/src/main/java/com/productos/repository/ProductoRepository.java
+++ producto/src/main/java/com/productos/repository/ProductoRepository.java
@@ -3,7 +3,11 @@ package com.productos.repository;
 import com.productos.entity.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 @Repository
 public interface ProductoRepository extends JpaRepository<Producto, Long> {
+    Optional<Producto> findByNombre(String nombre); 
+    
+    boolean existsByNombre(String nombre);
 }
