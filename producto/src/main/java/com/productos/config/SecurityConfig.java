1d5bd90504cdb961b7300c463c2b902fd03a7c8f
imgamp <gmojica@unal.edu.co>
2025-04-25 23:41:29 -0500

    Arreglo de security 2.0

--- producto/src/main/java/com/productos/config/SecurityConfig.java
+++ producto/src/main/java/com/productos/config/SecurityConfig.java
@@ -2,20 +2,23 @@ package com.productos.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/actuator/health", "/actuator/info").permitAll() // Deja pasar health sin auth
-                .anyRequest().authenticated() // Todo lo demás sigue protegido
+                .requestMatchers("/actuator/health", "/actuator/info", "/productos/saludo").permitAll()
+                .anyRequest().authenticated()
             )
-            .csrf(csrf -> csrf.disable()) // (opcional para APIs sin formulario)
+            .csrf(csrf -> csrf.disable())
             .build();
     }
 }
+
